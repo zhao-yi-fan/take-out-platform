4100df2857ce08f800c77df05e83a3e6e9f454b2
zhao-yi-fan <993445491@qq.com>
2023-04-13 10:05:56 +0000

    Deploying to gh-pages from @ zhao-yi-fan/take-out-platform@b1341b2015d315bc20cc7f4d433a62329ef46ef3 🚀

--- assets/collectionStore-40f774c8.js
+++
@@ -1,1 +0,0 @@
-import{B as o}from"./index-afba73bf.js";const s=o("collection",{state:()=>({collectionList:[{collectionId:1,userId:1,shopsIds:["2","3"]}]}),getters:{},actions:{setCollectionList(t){this.collectionList=t}},persist:!0});export{s as u};
---
+++ assets/collectionStore-fae6ae8f.js
@@ -0,0 +1,1 @@
+import{B as o}from"./index-fa9abe46.js";const s=o("collection",{state:()=>({collectionList:[{collectionId:1,userId:1,shopsIds:["2","3"]}]}),getters:{},actions:{setCollectionList(t){this.collectionList=t}},persist:!0});export{s as u};
